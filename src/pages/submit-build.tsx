8eac88f21d86b3471ccf6ca3626a633e06e25434
Kyle <kaplaugher94@gmail.com>
2023-01-28 17:47:28 -0500

    ts bs

---
+++ src/pages/submit-build.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+import { api } from "../utils/api";
+import { useState } from "react";
+import { useRouter } from "next/router";
+import { type NextPage } from "next";
+
+const SubmitBuildPage: NextPage = () => {
+  const createBuildMutation = api.builds.createBuild.useMutation();
+  console.log(createBuildMutation, "mutation");
+  const [build, setBuildOrder] = useState("");
+  const [matchUp, setMatchUp] = useState("ZvT");
+
+  const router = useRouter();
+
+  async function handleSubmitBuildOrder(e: React.FormEvent) {
+    e.preventDefault();
+    await createBuildMutation.mutateAsync({
+      matchUp,
+      build,
+    });
+    router.push("/");
+  }
+  return (
+    <div>
+      {" "}
+      <main className="flex min-h-screen  bg-gradient-to-b from-[#2e026d] to-[#15162c]  text-white">
+        <div className="mx-auto">
+          <form
+            onSubmit={handleSubmitBuildOrder}
+            className="flex h-full flex-col items-center justify-center gap-4"
+          >
+            <h1>Submit a build order</h1>
+            <label htmlFor="match-up-select">Match Up</label>
+            <select
+              id="match-up-select"
+              value={matchUp}
+              required
+              onChange={(e) => setMatchUp(e.target.value)}
+              className="p-2 text-black"
+            >
+              <option value="zvt">ZvT</option>
+              <option value="zvp">ZvP</option>
+              <option value="zvz">ZvZ</option>
+
+              <option value="pvt">PvT</option>
+              <option value="pvp">PvP</option>
+              <option value="pvz">PvZ</option>
+
+              <option value="tvt">TvT</option>
+              <option value="tvp">TvP</option>
+              <option value="tvz">TvZ</option>
+            </select>
+            <textarea
+              className="p-2 text-black"
+              value={build}
+              onChange={(e) => setBuildOrder(e.target.value)}
+            ></textarea>
+            <button type="submit">Submit</button>
+          </form>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default SubmitBuildPage;
